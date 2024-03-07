57bcd3f87044bba5caa034566b49c39712f0ee98
Ronald Arias <ariasron@hotmail.com>
2024-03-07 16:27:17 -0500

    creates svelte component and use it

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currentDate = new Date();
+  const currentHour = currentDate.getHours();
+
+  let greeting = "";
+
+  if(currentHour < 12) {
+    greeting = "Good morning";
+  } else if(currentHour < 18) {
+    greeting = "Good afternoon";
+  } else {
+    greeting = "Good night";
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{ greeting }</h1>
