ba94dd2d6b9d3f0fe220552f26b47b59701058e3
Ronald Arias <ariasron@hotmail.com>
2024-04-04 17:48:34 -0500

    shows the current song in the bottom

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,29 +9,48 @@ export const Pause = () => (
   <svg data-encore-id="icon" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" className="Svg-sc-ytk21e-0 dYnaPI"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
 )
 
+const CurrentSong = ({ image, title, artists }) => (
+  <div className="flex items-center gap-5 relative overflow-hidden">
+    <picture className="w-16 h-16 bg-zinc-800 roun shadow-lg overflow-hidden">
+      <img src={image} alt={title} />
+    </picture>
+    <div className="flex flex-col">
+      <h3 className="font-semibold text-sm black">
+        {title}
+      </h3>
+      <span className="text-xs opacity-80">
+        {artists?.join(', ')}
+      </span>
+    </div>
+  </div>
+)
+
 export function Player () {
-  const { isPlaying, setIsPlaying } = usePlayerStore(state => state);
-  const [currentSong, setCurrentSong] = useState(null);
+  const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(state => state);
   const audioRef = useRef();
 
   useEffect(() => {
-    audioRef.current.src = `/music/1/01.mp3`;
-  }, [])
+    isPlaying
+      ? audioRef.current.play()
+      : audioRef.current.pause();
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const { song, playlist, songs } = currentMusic;
+    if (song) {
+      const src = `/music/${playlist.id}/0${song.id}.mp3`;
+      audioRef.current.src = src;
+      audioRef.current.play();
+    }
+  }, [currentMusic])
 
   const handleClick = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-      audioRef.current.volume = 0.1;
-    }
-
     setIsPlaying(!isPlaying);
   }
 
   return (
     <div className="flex flex-row justify-between items-center w-full px-4 z-50">
-      <div>Current Song</div>
+      <CurrentSong {...currentMusic.song} />
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center">
           <button className="bg-white rounded-full p-2" onClick={handleClick}>
