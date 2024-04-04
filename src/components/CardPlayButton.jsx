fd280d71b93ac6b9128aa01ea259a139505befde
Ronald Arias <ariasron@hotmail.com>
2024-04-04 16:36:41 -0500

    set is playing only for the current playlist instead of all

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -2,11 +2,27 @@ import { usePlayerStore } from "@/store/playerStore";
 import { Pause, Play } from "./Player";
 
 export function CardPlayButton ({id}) {
-  const { isPlaying, setIsPlaying } = usePlayerStore(state => state);
+  const { 
+    currentMusic, 
+    isPlaying, 
+    setIsPlaying, 
+    setCurrentMusic 
+  } = usePlayerStore(state => state);
+
+  const handleClick = () => {
+    setCurrentMusic({
+      playlist: {
+        id
+      }
+    });
+    setIsPlaying(!isPlaying);
+  }
+
+  const isPlayingPlaylist = isPlaying && currentMusic.playlist.id === id;
 
   return (
-    <button className="bg-green-500 rounded-full p-4" onClick={() => setIsPlaying(!isPlaying)}>
-      {isPlaying ? <Pause /> : <Play />}
+    <button className="bg-green-500 rounded-full p-4" onClick={handleClick}>
+      {isPlayingPlaylist ? <Pause /> : <Play />}
     </button>
   )
 }
