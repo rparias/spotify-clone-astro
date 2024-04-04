81a9c4a3ce5fd33103a4850be16d2ebfcd4bede7
Ronald Arias <ariasron@hotmail.com>
2024-04-04 10:29:58 -0500

    adds an audio element to reproduce the music and persists on page navigation

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const Play = () => (
   <svg data-encore-id="icon" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" className="Svg-sc-ytk21e-0 dYnaPI"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
@@ -10,17 +10,36 @@ const Pause = () => (
 
 export function Player () {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(null);
+  const audioRef = useRef();
+
+  useEffect(() => {
+    audioRef.current.src = `/music/1/01.mp3`;
+  }, [])
+
+  const handleClick = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+      audioRef.current.volume = 0.1;
+    }
+
+    setIsPlaying(!isPlaying);
+  }
   return (
     <div className="flex flex-row justify-between items-center w-full px-4 z-50">
       <div>Current Song</div>
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center">
-          <button className="bg-white rounded-full p-2" onClick={() => setIsPlaying(!isPlaying)}>
+          <button className="bg-white rounded-full p-2" onClick={handleClick}>
             {isPlaying ? <Pause /> : <Play />}
           </button>
         </div>
       </div>
       <div>Volumen</div>
+
+      <audio ref={audioRef} />
     </div>
   )
 }
