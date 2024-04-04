5c5fc6babbce16b32812de54801e21849f30b971
Ronald Arias <ariasron@hotmail.com>
2024-04-04 15:41:22 -0500

    adds a play button on the playlist item card component

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,9 @@
+import { Pause, Play } from "./Player"
+
+export function CardPlayButton ({id}) {
+  return (
+    <button className="bg-green-500 rounded-full p-4" onClick={() => {}}>
+      <Play />
+    </button>
+  )
+}
