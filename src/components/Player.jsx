89a056e93d67867c321522a205de44e0893e9678
Ronald Arias <ariasron@hotmail.com>
2024-04-05 10:44:11 -0500

    allows to mute the volumen clicking on the volumen icon

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -37,15 +37,30 @@ const CurrentSong = ({ image, title, artists }) => (
 const VolumeControl = () => {
   const volume = usePlayerStore(state => state.volume);
   const setVolume = usePlayerStore(state => state.setVolume);
+  const previousVolumeRef = useRef(volume);
+
+  const isVolumeSilenced = volume < 0.1;
+
+  const handleClickVolume = () => {
+    if (isVolumeSilenced) {
+      setVolume(previousVolumeRef.current);
+    } else {
+      previousVolumeRef.current = volume;
+      setVolume(0);
+    }
+  }
 
   return (
     <div className="flex justify-center gap-x-2">
-      { volume < 0.1 ? <VolumeSilence /> : <Volume /> }
+      <button onClick={handleClickVolume}>
+        { isVolumeSilenced ? <VolumeSilence /> : <Volume /> }
+      </button>
       <Slider
         defaultValue={[100]}
         max={100}
         min={0}
         className="w-[95px]"
+        value={[volume * 100]}
         onValueChange={(value) => {
           const [newVolume] = value;
           const volumeValue = newVolume / 100;
